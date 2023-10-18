51e2898c8228584385106485aeda0d1c9881d819
Eric Niemeyer <eric.niemeyer@venturetec.net>
2023-10-18 06:46:58 -0400

    add like

--- src/navigation/Main.tsx
+++ src/navigation/Main.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
 import {Home} from '../screens/Home';
+import LikesScreen from '../screens/LikesScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export const MainNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Likes" component={LikesScreen} />{' '}
       </Stack.Navigator>
     </NavigationContainer>
   );
