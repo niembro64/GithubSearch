948fc0a1f4716ecfb0e806d6a46b122d7b127a5a
Eric Niemeyer <eric.niemeyer@venturetec.net>
2023-10-19 04:03:34 -0400

    more model

--- src/RootStore.ts
+++ src/RootStore.ts
@@ -4,3 +4,6 @@ import {LikesStoreModel} from './LikesStore';
 export const RootStoreModel = types.model('RootStore').props({
   likesStore: types.optional(LikesStoreModel, {}),
 });
+
+export type RootStore = typeof RootStoreModel.Type;
+export type RootStoreSnapshot = typeof RootStoreModel.SnapshotType;
