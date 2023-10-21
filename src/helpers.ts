5c5ce1acb06282e588ba44be7990751027433d73
Eric Niemeyer <eric.niemeyer@venturetec.net>
2023-10-21 06:33:19 -0400

    random

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {RepoGithubSmall, SortStars} from './types';
+
 export const truncateString = (str: any, maxLength: number) => {
   if (typeof str !== 'string') {
     return '';
@@ -7,3 +10,20 @@ export const truncateString = (str: any, maxLength: number) => {
 };
 
 export const keyboardVerticalOffsetIOS = 72;
+
+export const sortResults = (
+  r: RepoGithubSmall[],
+  d: SortStars,
+): RepoGithubSmall[] => {
+  switch (d) {
+    case 'none':
+      // RANDOM SORT
+      return r.sort((a, b) => 0.5 - Math.random());
+    case 'asc':
+      return r.sort((a, b) => a.stargazers_count - b.stargazers_count);
+    case 'desc':
+      return r.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    default:
+      throw new Error('sortStars is not a valid value');
+  }
+};
