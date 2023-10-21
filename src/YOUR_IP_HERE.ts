da8ad7752536946f8be06f47fcddc52df6fd7743
Eric Niemeyer <eric.niemeyer@venturetec.net>
2023-10-21 02:17:25 -0400

    comment

--- src/YOUR_IP_HERE.ts
+++ src/YOUR_IP_HERE.ts
@@ -1,5 +1,10 @@
 export const numAllowedLikes = 2;
 export const numSearchResults = 3;
 
+///////////////////////////////////////////////
+// Change "myIp" to your machine's local IP address
+// i.e. The machine running the server
+///////////////////////////////////////////////
+
 // export const myIp = '192.168.86.28';
 export const myIp = '192.168.1.19';
